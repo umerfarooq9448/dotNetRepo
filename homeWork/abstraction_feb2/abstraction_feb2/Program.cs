ee6c181786faff4135544aed7c263d73812cfe53
umerfarooq9448 <umerfarooq9448@gmail.com>
2023-02-02 23:48:39 +0530

    commiting the final homework

---
+++ homeWork/abstraction_feb2/abstraction_feb2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction_feb2
+{
+    abstract class phone{
+        public abstract void calling();
+        public abstract void texting();
+        }
+    class smartphone : phone
+    {
+        public override void calling()
+        {
+            Console.WriteLine("calling");
+        }
+
+        public override void texting()
+        {
+            Console.WriteLine("texting");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            smartphone s1 = new smartphone();
+            s1.calling();
+            s1.texting();
+        }
+    }
+}
---
+++ homeWork/inheritance_feb2/inheritance_feb2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_feb2
+{
+    class phone
+    {
+        public void calling()
+        {
+            Console.WriteLine("calling.....");
+        }
+        public void texting()
+        {
+            Console.WriteLine("texting.....");
+        }
+
+    }
+
+    class smartphone : phone            // example of inheritance
+    {
+        public void camera()
+        {
+            Console.WriteLine("opening camera.....");
+        }
+    }
+
+    class galaxy : smartphone               // example of multi level inheritance
+    {
+        public void hdRecording()
+        {
+            Console.WriteLine("hd recording.....");
+        }
+    }
+
+    class smartphone2: phone        // example of multiple inheritance
+    {
+        public void lowResCamera() {
+
+            Console.WriteLine("camera of low resoution....");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            smartphone s1 = new smartphone();   
+            smartphone2 s2 = new smartphone2();
+            galaxy g3 = new galaxy();
+            s1.camera();
+            s2.lowResCamera();
+            g3.hdRecording();
+
+        }
+    }
+}
