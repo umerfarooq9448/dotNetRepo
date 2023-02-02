38298ec7a5434068a9559479eb5cdc2c83c37c61
umerfarooq9448 <umerfarooq9448@gmail.com>
2023-02-02 23:31:45 +0530

    pushing homework

---
+++ homeWork/polymorphism_function_overloading_feb2/polymorphism_function_overloading_feb2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_function_overloading_feb2
+{   
+    class operation
+    {
+        public void add(int a,int b)
+        {
+            Console.WriteLine(a+b);
+        }
+
+        public void add(int a, int b, int c)   //-------> exmaple of method overloading
+        {
+            Console.WriteLine(a + b + c);
+        }
+
+    }
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            operation op = new operation();
+            op.add(1,2);
+            op.add(2,3,2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homeWork/polymorphism_overriding_feb2/polymorphism_overriding_feb2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_overriding_feb2
+{
+    class operation
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+    }
+    class child_operations:operation
+    {
+        public void add(int a, int b, int c) {
+
+            Console.WriteLine(a + b+c);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            child_operations op = new child_operations();
+            
+            op.add(2, 3);
+            op.add(3, 4,1);
+            Console.ReadLine();
+        }
+    }
+}
